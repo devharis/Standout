8f22dc89667fc20cac1d34c00b264355352f6791
Haris <haris.kljajic@hotmail.se>
2013-11-10 02:39:48 +0100

    Added JS for getting latitude, longitude from html

--- SWTS/SWTS/Scripts/mapLoader.js
+++ SWTS/SWTS/Scripts/mapLoader.js
@@ -1,16 +1,28 @@
 ﻿function initialize() {
+
+    var lat = parseFloat($("div.supplierInfo .mapLat").attr("name"));
+    var lng = parseFloat($("div.supplierInfo .mapLng").attr("name"));
+    var supplier = $("div.supplier h2").html();
+    
+    var latAndLng = new google.maps.LatLng(lat, lng);
     var mapOptions = {
-        zoom: 8,
-        center: new google.maps.LatLng(-34.397, 150.644),
+        zoom: 16,
+        center: latAndLng,
         mapTypeId: google.maps.MapTypeId.ROADMAP
     }
     var map = new google.maps.Map(document.getElementById("map-canvas"), mapOptions);
+    
+    var marker = new google.maps.Marker({
+        position: latAndLng,
+        map: map,
+        title: supplier
+    });
 }
 
 function loadScript() {
     var script = document.createElement("script");
     script.type = "text/javascript";
-    script.src = "http://maps.googleapis.com/maps/api/js?key=AIzaSyCV6pBeEmMNqozllUvxpKZgkf0YME469pE&sensor=false&callback=initialize";
+    script.src = "http://maps.googleapis.com/maps/api/js?key=AIzaSyCV6pBeEmMNqozllUvxpKZgkf0YME469pE&sensor=true&callback=initialize";
     document.body.appendChild(script);
 }
 
