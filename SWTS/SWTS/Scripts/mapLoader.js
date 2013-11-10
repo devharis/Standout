e12ed7b3f8a0f4f5b7a764fdd222b17abaa93443
Haris <haris.kljajic@hotmail.se>
2013-11-10 22:08:39 +0100

    Adding supplier done, with dynamic maplocator

--- SWTS/SWTS/Scripts/mapLoader.js
+++ SWTS/SWTS/Scripts/mapLoader.js
@@ -1,5 +1,7 @@
-﻿function initialize() {
-
+﻿var geocoder;
+var map;
+function initialize() {
+    geocoder = new google.maps.Geocoder();
     var lat = parseFloat($("div.supplierInfo .mapLat").attr("name"));
     var lng = parseFloat($("div.supplierInfo .mapLng").attr("name"));
     var supplier = $("div.supplier h2").html();
@@ -10,7 +12,7 @@
         center: latAndLng,
         mapTypeId: google.maps.MapTypeId.ROADMAP
     }
-    var map = new google.maps.Map(document.getElementById("map-canvas"), mapOptions);
+    map = new google.maps.Map(document.getElementById("map-canvas"), mapOptions);
     
     var marker = new google.maps.Marker({
         position: latAndLng,
@@ -18,6 +20,42 @@
         title: supplier
     });
 }
+
+function codeAddress(addressInput, cityInput, countryInput) {
+
+    if (typeof (addressInput) === 'undefined') addressInput = "";
+    if (typeof (cityInput) === 'undefined') cityInput = "";
+    if (typeof (countryInput) === 'undefined') countryInput = "";
+
+    var address = addressInput + " " + cityInput + " " + countryInput;
+
+    geocoder.geocode({ 'address': address }, function (results, status) {
+        if (status == google.maps.GeocoderStatus.OK) {
+            map.setCenter(results[0].geometry.location);
+            var marker = new google.maps.Marker({
+                map: map,
+                position: results[0].geometry.location
+            });
+            
+            $(".supplier .lat").attr("value", results[0].geometry.location.lat().toString().replace(".", ","));
+            $(".supplier .lng").attr("value", results[0].geometry.location.lng().toString().replace(".", ","));
+        }
+    });
+}
+
+$('.geocodeAddress').change('input', function () {
+    if ($(".geocodeAddress").attr("value").length >= 3)
+    codeAddress($('.geocodeAddress').attr("value"), $('.geocodeCity').attr("value"), $('.geocodeCountry').attr("value"));
+});
+$('.geocodeCity').change('input', function () {
+    if ($(".geocodeCity").attr("value").length >= 3)
+        codeAddress($('.geocodeAddress').attr("value"), $('.geocodeCity').attr("value"), $('.geocodeCountry').attr("value"));
+});
+$('.geocodeCountry').change('input', function () {
+    if ($(".geocodeCountry").attr("value").length >= 3)
+        codeAddress($('.geocodeAddress').attr("value"), $('.geocodeCity').attr("value"), $('.geocodeCountry').attr("value"));
+});
+
 
 function loadScript() {
     var script = document.createElement("script");
